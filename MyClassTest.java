eb58e7b67475986901d48875c2a7b616b751ab25
Humphreys-Erin <woo13069@byui.edu>
2018-11-08 20:13:30 -0500

    1st test that fails

---
+++ MyClassTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myclass;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author woo13
+ */
+public class MyClassTest {
+    MyClass my;
+    public MyClassTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        my = new MyClass();
+    }
+    
+    @After
+    public void tearDown() {
+        my = null;
+    }
+
+    @Test
+    public void testadd() {
+        int a=20,b=10,c;
+        c = my.add(a, b);
+        assertEquals(40,c);
+    }
+    
+}
